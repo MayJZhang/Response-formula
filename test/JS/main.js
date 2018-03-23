26c7fb903daa02ebee9a0849d81a642285b3e7f7
MayJZhang <MayJZhang@users.noreply.github.com>
2018-03-23 17:55:07 +0800

    update

--- test/JS/main.js
+++ test/JS/main.js
@@ -1,8 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-$("#intro .scrolly").scrolly();
-$("#navPanelToggle").click(function(){
+  $("#intro .scrolly").scrolly();
 
-});
+  $("#navPanelToggle").click(function() {
+    $("#navPanel").css("visibility","visible");
+    $("#navPanel").css("right","0");
+    //$(document.body).
+  });
 
+  $("#navPanel .close").click(function() {
+    $("#navPanel").css("visibility","hidden");
+    $("#navPanel").css("right","-100%");
+  });
 });
